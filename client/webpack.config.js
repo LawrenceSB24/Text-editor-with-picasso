14e87a9498b3c31a9f151693e19fc72652f1130e
LawrenceSB24 <lawrs2022@gmail.com>
2022-05-07 00:15:38 -0400

    Files for webpack config and additional client functionality

---
+++ client/webpack.config.js
@@ -0,0 +1,83 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      // Plugins for text editor
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        // title: 'Text Editor'
+        title: 'Contact Cards'
+      }),
+
+      // Plugin for injecting service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+
+      // Plugin for creating a manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Text Editor for creating notes and code snippets',
+        background_color: '#000000',
+        theme_color: '#ff0000',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            size: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
+    ],
+
+    module: {
+      rules: [
+        // CSS loaders
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        // Test for......images? Im not sure
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
+        },
+        // JS loaders
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            // Babel-loader for using ES6
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        }
+      ],
+    },
+  };
+};
