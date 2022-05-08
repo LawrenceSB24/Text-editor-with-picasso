20e31285adcc8fc33e234b629fdce48cb4002ce5
LawrenceSB24 <lawrs2022@gmail.com>
2022-05-07 21:44:16 -0400

    Testing out quotation edit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,33 +20,33 @@ module.exports = () => {
     plugins: [
       // Plugins for text editor
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE'
+        template: "./index.html",
+        title: "JATE"
         // title: 'Contact Cards'
       }),
 
       // Plugin for injecting service worker
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js'
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js"
       }),
 
       // Plugin for creating a manifest file
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'Text Editor for creating notes and code snippets',
-        background_color: '#000000',
-        theme_color: '#ff0000',
-        start_url: '/',
-        publicPath: '/',
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Text Editor for creating notes and code snippets",
+        background_color: "#000000",
+        theme_color: "#ff0000",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             size: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons')
+            destination: path.join("assets", "icons")
           }
         ]
       })
@@ -70,10 +70,10 @@ module.exports = () => {
           exclude: /node_modules/,
           use: {
             // Babel-loader for using ES6
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
             }
           }
         }
