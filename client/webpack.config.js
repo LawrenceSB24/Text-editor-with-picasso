53bd9821cb417a84e8104fa8ba3514cb8291dcd8
LawrenceSB24 <lawrs2022@gmail.com>
2022-05-07 22:02:43 -0400

    Hopefully fixed webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = () => {
         short_name: "JATE",
         description: "Text Editor for creating notes and code snippets",
         background_color: "#225ca3",
-        theme_color: "##225ca3",
+        theme_color: "#225ca3",
         start_url: "/",
         publicPath: "/",
         icons: [
